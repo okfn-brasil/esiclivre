a98e9ad98668022566714c34d2ed55d4f121146f
Alexandre Souza <alexandre_@fastmail.com>
2015-10-10 10:35:44 -0300

    adding PrePedido model

--- esiclivre/models.py
+++ esiclivre/models.py
@@ -2,6 +2,8 @@
 
 from __future__ import unicode_literals  # unicode by default
 
+import datetime
+
 import sqlalchemy as sa
 import sqlalchemy_utils as sa_utils
 
@@ -48,6 +50,68 @@ class PedidosUpdate(db.Model):
     date = db.Column(sa_utils.ArrowType, index=True)
 
 
+class PrePedido(db.Model):
+
+    __tablename__ = 'pre_pedido'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    author_id = db.Column(db.Integer)
+
+    orgao_name = db.Column(db.String(255))
+
+    text = db.Column(sa.UnicodeText())
+
+    keywords = db.Column(db.String(255))  # separated by commas
+
+    state = db.Column(db.String(255))  # WAITING or PROCESSED
+
+    created_at = db.Column(sa_utils.ArrowType)
+
+    updated_at = db.Column(sa_utils.ArrowType)
+
+    @classmethod
+    def get_all_pending(self):
+        return self.query.filter_by(state='WAITING')
+
+    @property
+    def orgao(self):
+        return Orgao.query.filter_by(name=self.orgao_name).one()
+
+    @property
+    def author(self):
+        return Author.query.filter_by(id=self.author_id).one()
+
+    @property
+    def all_keywords(self):
+        return [
+            Keyword.query.filter_by(name=k).one() for k in self.keywords.split(',')  #noqa
+        ]
+
+    def create_pedido(self, protocolo, deadline):
+
+        pedido = Pedido()
+
+        pedido.protocol = protocolo
+        pedido.deadline = deadline
+
+        pedido.orgao = self.orgao
+        pedido.author = self.author
+        pedido.keywords = self.all_keywords
+
+        pedido.description = self.text
+        pedido.request_date = datetime.datetime.today()
+
+        db.session.add(pedido)
+        db.session.commit()
+
+        self.updated_at = datetime.datetime.today()
+        self.state = 'PROCESSED'
+
+        db.session.add(self)
+        db.session.commit()
+
+
 class Pedido(db.Model):
 
     __tablename__ = 'pedido'
@@ -80,8 +144,8 @@ class Pedido(db.Model):
         'Author', secondary=pedido_author, backref='pedidos', uselist=False
     )
 
-    keyword = db.relationship(
-        'Keyword', secondary=pedido_keyword, backref='pedido', uselist=False
+    keywords = db.relationship(
+        'Keyword', secondary=pedido_keyword, backref='pedido'
     )
 
     attachments = db.relationship(
