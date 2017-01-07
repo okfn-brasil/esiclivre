444a9be7dee1c3ab4bc1ddbe9073ae4436132001
AndresMRM <andres@inventati.org>
2017-01-07 09:46:19 -0200

    allow settings folder path to be set using manage.py

--- manage.py
+++ manage.py
@@ -9,14 +9,18 @@ from flask.ext.migrate import Migrate, MigrateCommand
 
 from esiclivre.app import create_app, db, sv
 
-app = create_app()
-manager = Manager(app)
+manager = Manager(create_app)
+
+manager.add_option(
+    "-s", "--settings", dest="settings_folder", required=False,
+    default=os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'settings'))
 
 # Migrations
-migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
-
-# manager.add_command('run', Server(port=5004))
+# TODO: comentando as duas linhas abaixo por enquanto, pois não sei
+# o que passar como app uma vez que ela vai ser criada pelo Manager depois.
+# migrate = Migrate(app, db)
+# manager.add_command('db', MigrateCommand)
 
 # Shell
 manager.add_command('shell', Shell(make_context=lambda: {
